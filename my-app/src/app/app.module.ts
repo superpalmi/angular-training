d6c131221a274d636774c82bc26321748d056b31
riccardopalmieri95 <68059318+riccardopalmieri95@users.noreply.github.com>
2020-08-18 17:52:31 +0200

    added user component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
+import { RentalComponent } from './rental/rental.component';
+import {RouterModule, Routes} from '@angular/router';
+import { UserComponent } from './user/user.component';
+
+const routes: Routes = [
+  {path: '', component: RentalComponent},
+  {path: 'user', component: UserComponent}
+
+
+];
 // qui dichiaro i moduli appena creati
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RentalComponent,
+    UserComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
