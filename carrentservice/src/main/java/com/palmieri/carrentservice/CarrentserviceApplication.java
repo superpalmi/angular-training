4c0b1c4f39a5e6ce53ba7fc3e266611bb1e62b85
riccardopalmieri95 <68059318+riccardopalmieri95@users.noreply.github.com>
2020-09-02 17:05:07 +0200

    Added spring boot

---
+++ carrentservice/src/main/java/com/palmieri/carrentservice/CarrentserviceApplication.java
@@ -0,0 +1,13 @@
+package com.palmieri.carrentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrentserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrentserviceApplication.class, args);
+	}
+
+}
---
+++ carrentservice/src/test/java/com/palmieri/carrentservice/CarrentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palmieri.carrentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
